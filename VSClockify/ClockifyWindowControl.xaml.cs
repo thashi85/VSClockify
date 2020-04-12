833911a7b4e278ff8fccae10b24dcef51f2d130f
Thakshila <thahsi.ucsc@gmail.comm>
2020-04-12 23:52:37 +0530

    ui changes

--- VSClockify/ClockifyWindowControl.xaml.cs
+++ VSClockify/ClockifyWindowControl.xaml.cs
@@ -41,8 +41,8 @@ namespace VSClockify
         {
             try
             {
-                MethodLogger.SaveLogToFile("ClockifyWindowControl ClockifyApiKey:" + ServiceUtility.ClockifyApiKey);
-                MethodLogger.SaveLogToFile("ClockifyWindowControl ClockifyApiUrl:" + ServiceUtility.ClockifyApiUrl);
+                //MethodLogger.SaveLogToFile("ClockifyWindowControl ClockifyApiKey:" + ServiceUtility.ClockifyApiKey);
+                //MethodLogger.SaveLogToFile("ClockifyWindowControl ClockifyApiUrl:" + ServiceUtility.ClockifyApiUrl);
                 this.InitializeComponent();
                 this._clockifyService = new ClockyifyService();
                 textBox_apiKey.Text = ServiceUtility.ClockifyApiKey;
@@ -69,45 +69,64 @@ namespace VSClockify
         /// <param name="e">The event args.</param>
         private void Timer_Click(object sender, RoutedEventArgs e)
         {
-            try { 
-            if (textBox_apiKey.Text.Length == 0)
-            {
-                MessageBox.Show("Please fill the Clockify API Key");
-            }else if (textBoxDesc.Text.Length == 0)
-            {
-                MessageBox.Show("Please add description");
-            }
-            else if (!_timerStart)
-            {
-                var _res=_clockifyService.PostTimeEntry(workspaceId, new TimeEntryRequest()
+           
+            try {
+               // buttonApply.Content = !_timerStart ? "Starting..." : "Stopping...";
+                //progressbar.Visibility = Visibility.Visible;
+                //progressbar.Value = 5;
+                if (textBox_apiKey.Text.Length == 0)
                 {
-                    start = DateTime.Now,
-                    description= textBoxDesc.Text
-                });
-                if (!string.IsNullOrEmpty(_res?.id))
-                {
-                    _timerStart = !_timerStart;
-                    buttonTimer.Content = "Stop Timer";
+                    MessageBox.Show("Please fill the Clockify API Key");
                 }
-            }
-            else
-            {
-                var _res =_clockifyService.StopTimeEntry(workspaceId,_user.id, new StopTimeEntryRequest()
+                else if (textBoxDesc.Text.Length == 0)
                 {
-                    end = DateTime.Now                  
-                });
-                if (!string.IsNullOrEmpty(_res?.id))
-                {
-                    _timerStart = !_timerStart;
-                    buttonTimer.Content = "Start Timer";
+                    MessageBox.Show("Please add description");
                 }
-            }
+                else if (comboBoxProject.SelectedValue.ToString().Length == 0)
+                {
+                    MessageBox.Show("Please select a project");
+                }
+                else if (!_timerStart)
+                {
+                   
+                    //progressbar.Value = 30;
+                   
+                    var _res = _clockifyService.PostTimeEntry(workspaceId, new TimeEntryRequest()
+                    {
+                        start = DateTime.UtcNow,
+                        description = textBoxDesc.Text,
+                        projectId = comboBoxProject.SelectedValue.ToString()
+                    });
+                    //progressbar.Value = 90;
+                    if (!string.IsNullOrEmpty(_res?.id))
+                    {
+                        _timerStart = !_timerStart;
+                        buttonTimer.Content = "Stop Timer";
+                    }
+                }
+                else
+                {
+                    var _res = _clockifyService.StopTimeEntry(workspaceId, _user.id, new StopTimeEntryRequest()
+                    {
+                        end = DateTime.UtcNow
+                    });
+                    if (!string.IsNullOrEmpty(_res?.id))
+                    {
+                        _timerStart = !_timerStart;
+                        buttonTimer.Content = "Start Timer";
+                    }
+                }
+
+                //progressbar.Visibility = Visibility.Hidden;
             }
             catch (Exception ex)
             {
+                //buttonApply.Content = !_timerStart ? "Start Timer" : "Stop Timer";
+                //progressbar.Visibility = Visibility.Hidden;
                 MethodLogger.SaveLogToFile("Timer_Click exception:" + ex.Message);
                 MethodLogger.SaveLogToFile("Timer_Click exception:" + ex.StackTrace);
             }
+           
             //MessageBox.Show(
             // string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", textBox.Text),
             // "ClockifyWindow");
@@ -117,23 +136,32 @@ namespace VSClockify
         {
             try
             {
+                //progressbar.Visibility = Visibility.Visible;
+                //progressbar.Value = 5;
+                //buttonApply.Content = "Processing...";
                 if (textBox_apiKey.Text.Length == 0)
                 {
                     MessageBox.Show("Please fill the Clockify API Key");
                 }
                 else
                 {
-
+                    //progressbar.Value = 15;
+                    UCLoadingControl.Visibility = Visibility.Visible;
                     ServiceUtility.ClockifyApiKey = textBox_apiKey.Text;
                     loadClockifyData();
+                    UCLoadingControl.Visibility = Visibility.Hidden;
                     if (string.IsNullOrEmpty(_user?.id))
                     {
                         MessageBox.Show("Invalild Clockify API Key");
                     }
                 }
+                //buttonApply.Content = "Apply";
+                //progressbar.Visibility = Visibility.Hidden;
             }
             catch (Exception ex)
             {
+                //buttonApply.Content = "Apply";
+                //progressbar.Visibility = Visibility.Hidden;
                 MethodLogger.SaveLogToFile("buttonApply_Click exception:" + ex.Message);
                 MethodLogger.SaveLogToFile("buttonApply_Click exception:" + ex.StackTrace);
             }
@@ -142,6 +170,7 @@ namespace VSClockify
         private void loadClockifyData()
         {
             _user = _clockifyService.GetUser();
+            //progressbar.Value = 45;
             if (!string.IsNullOrEmpty(_user?.id))
             {
                 labelTitle.Content = _user.name;
@@ -149,6 +178,7 @@ namespace VSClockify
                 // SetupPanel.Visibility = Visibility.Collapsed;
 
                 _projects = _clockifyService.GetProjects(workspaceId);
+               // progressbar.Value = 80;
                 if ((_projects?.Count ?? 0) >0)
                 {
                     comboBoxProject.Items.Clear();
